5cccc263b720c35fbe8f9fd225e254ab2a5744b4
Essam Alsaud <essamx123@gmail.com>
2023-04-27 12:33:33 +0300

    Need to calculate the age

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 /* Essam Alsaud 1636456 */
+alert("Name: Essam Alsaud \nID: 1636456");
+
+function f(n) {
+    if (n == 0) {
+        return 1
+    } else {
+        return f(n - 1) * n
+    }
+}
+console.log("Factorial 4 is: "+f(4))
+
+function Check(){
+    
+}
+
+
+
